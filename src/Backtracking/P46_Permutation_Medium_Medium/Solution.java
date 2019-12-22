8b298d4f14be99df547e345fa09e1529520c8fd4
Chris Qin <chriscode3@Chriss-MBP.hsd1.wa.comcast.net>
2019-12-21 21:48:03 -0800

    day 1

---
+++ src/Backtracking/P46_Permutation_Medium_Medium/Solution.java
@@ -0,0 +1,33 @@
+package Backtracking.P46_Permutation_Medium_Medium;
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backTracking(res, new ArrayList<>(), nums);
+
+        return res;
+    }
+
+    private void backTracking(List<List<Integer>> res, List<Integer> tempList, int[] nums) {
+        if(tempList.size() == nums.length) {
+            res.add(new ArrayList<>(tempList));
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(tempList.contains(nums[i])) {
+                continue;
+            } else {
+                tempList.add(nums[i]);
+                backTracking(res, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = new int[]{1,2,3};
+        System.out.println(s.permute(nums));
+    }
+}
