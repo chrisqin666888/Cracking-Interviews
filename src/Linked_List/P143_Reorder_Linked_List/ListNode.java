29b8e19ddff7edae036e2011419adaa2b8fdbef3
chriscode3 <chrisqcode3@gmail.com>
2019-12-27 21:25:48 -0800

    day 7

---
+++ src/Linked_List/P143_Reorder_Linked_List/ListNode.java
@@ -0,0 +1,11 @@
+package Linked_List.P143_Reorder_Linked_List;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
