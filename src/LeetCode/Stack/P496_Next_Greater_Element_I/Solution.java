311668183033375f044af5e5d0807216c0a267c4
chriscode3 <chrisqcode3@gmail.com>
2020-01-03 22:40:55 -0800

    day 14

---
+++ src/LeetCode/Stack/P496_Next_Greater_Element_I/Solution.java
@@ -0,0 +1,17 @@
+package LeetCode.Stack.P496_Next_Greater_Element_I;
+import java.util.*;
+
+public class Solution {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }
+        for (int i = 0; i < findNums.length; i++)
+            findNums[i] = map.getOrDefault(findNums[i], -1);
+        return findNums;
+    }
+}
